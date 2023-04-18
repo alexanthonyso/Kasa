a8754a479380ff8896664683bf32f2c0feff735e
alexanthonyso <soufflet.alexis@gmail.com>
2023-04-18 10:09:38 +0200

    Create pages & components

---
+++ src/components/Locations.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+
+const Locations = () => {
+    // const [locationsData, setLocationsState] = React.useState(locations)
+    // return (
+    //     {
+    //         locationsData.map(({location, id}) => (
+    //             <Card key={id}/>
+    //         ))
+    //     }
+    // )
+}
+
+export default Locations
